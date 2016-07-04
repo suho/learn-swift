971d3e720953ad55431a63bf7e07af4e0df5f37f
Ho Van Su <hovansu.it@gmail.com>
2016-07-04 17:05:41 +0700

    Finished Login UI

--- Project/SFoodProject/SFoodProject/LoginViewController.swift
+++ Project/SFoodProject/SFoodProject/LoginViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.navigationController?.navigationBarHidden = true
+        self.usernameTextField.delegate = self
+        self.passwordTextField.delegate = self
         // Do any additional setup after loading the view.
     }
 
@@ -21,15 +25,74 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(animated: Bool) {
+        self.usernameTextField.text = ""
+        self.passwordTextField.text = ""
     }
-    */
+    
+    @IBAction func loginAction(sender: AnyObject) {
+        let username = self.usernameTextField.text!
+        let password = self.passwordTextField.text!
+        
+        if self.checkAccount(username, password: password) {
+            print("Dang nhap thanh cong")
+            //Will Push Navi Here
+        } else {
+            if self.checkValid(username, password: password) {
+                self.showMessage("Input Not Valid", viewController: self)
+            } else {
+                self.showMessage("User Not Available", viewController: self)
+            }
+        }
+    }
+    @IBAction func forgotPasswordAction(sender: AnyObject) {
+        print("Quen Mat Khau")
+        //Will Push Navi Here
+    }
+    @IBAction func registerAction(sender: AnyObject) {
+        print("Dang Ky")
+        //Will Push Navi Here
+    }
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        
+        if touch.view == self.view {
+            self.usernameTextField.resignFirstResponder()
+            self.passwordTextField.resignFirstResponder()
+        }
+    }
+    
+    func checkAccount(username: String, password: String) -> Bool {
+        if username == "admin" && password == "admin" {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func checkValid(username: String, password: String) -> Bool {
+        if username == "" && password == "" {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func showMessage(message: String, viewController: UIViewController) {
+        let alertController = UIAlertController(title: "Message", message: "\(message)", preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        viewController.presentViewController(alertController, animated: true, completion: nil)
+    }
 
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if textField == self.usernameTextField {
+            self.passwordTextField.becomeFirstResponder()
+        } else {
+            self.loginAction(self)
+        }
+        return true
+    }
 }
