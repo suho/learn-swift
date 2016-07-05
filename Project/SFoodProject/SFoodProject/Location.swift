8eca7a5d751c85e6c5b9b9b438f23a91e77130c0
Ho Van Su <hovansu.it@gmail.com>
2016-07-05 16:54:14 +0700

    add file plist and model location

---
+++ Project/SFoodProject/SFoodProject/Location.swift
@@ -0,0 +1,28 @@
+//
+//  Location.swift
+//  SFoodProject
+//
+//  Created by Mylo Ho on 7/5/16.
+//  Copyright © 2016 Ho Van Su. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Location {
+    var images: [String] = [String]()
+    var name: String = ""
+    var address: String = ""
+    var previewText: String = ""
+    var detailText: String = ""
+    var coordinates: (Double, Double) = (0.0, 0.0)
+    
+    init(images: [String], name: String, address: String, previewText: String, detailText: String, coordinates: (Double, Double)) {
+        self.images = images
+        self.name = name
+        self.address = address
+        self.previewText = previewText
+        self.detailText = detailText
+        self.coordinates = coordinates
+    }
+}
