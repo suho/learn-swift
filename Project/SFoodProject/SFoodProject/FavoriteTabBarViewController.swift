91f3562ec35c940f8d1b4dc23dfca8b70487e4c5
Ho Van Su <hovansu.it@gmail.com>
2016-07-06 09:50:14 +0700

    finished custom cell for favorite table view

---
+++ Project/SFoodProject/SFoodProject/FavoriteTabBarViewController.swift
@@ -0,0 +1,27 @@
+//
+//  FavoriteTabBarViewController.swift
+//  SFoodProject
+//
+//  Created by Mylo Ho on 7/6/16.
+//  Copyright © 2016 Ho Van Su. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteTabBarViewController: UIViewController {
+
+    @IBOutlet weak var favoriteTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
