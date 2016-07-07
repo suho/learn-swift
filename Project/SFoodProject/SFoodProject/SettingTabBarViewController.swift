2706c45dc0c77ec7712febb14a52d803fde11b4a
Ho Van Su <hovansu.it@gmail.com>
2016-07-07 08:37:11 +0700

    fix selection on table view of settings

--- Project/SFoodProject/SFoodProject/SettingTabBarViewController.swift
+++ Project/SFoodProject/SFoodProject/SettingTabBarViewController.swift
@@ -12,12 +12,15 @@ class SettingTabBarViewController: UIViewController {
 
     @IBOutlet weak var settingsTableView: UITableView!
     
-    let sections = ["Account", "Peoples"]
+    let sections = ["Account", "Peoples", "Logout"]
+    let items = [["Username", "Email", "Profile Details"], ["Following", "Follower"], ["Logout"]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Settings"
-        // Do any additional setup after loading the view.
+        self.settingsTableView.delegate = self
+        self.settingsTableView.dataSource = self
+        self.settingsTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "tableCell")
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,4 +29,44 @@ class SettingTabBarViewController: UIViewController {
     }
     
 
+}
+
+extension SettingTabBarViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return self.sections.count
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items[section].count
+    }
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.sections[section]
+    }
+    func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0
+    }
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 30
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("tableCell", forIndexPath: indexPath)
+        cell.tintColor = UIColor.orangeColor()
+        cell.textLabel?.text = self.items[indexPath.section][indexPath.row]
+        cell.userInteractionEnabled = false
+        if (indexPath.section == 0 && indexPath.row == 2) || (indexPath.section == 1 && indexPath.row != 2) || (indexPath.section == 2) {
+            cell.userInteractionEnabled = true
+        }
+        return cell
+    }
+    func tableView(tableView: UITableView, accessoryButtonTappedForRowWithIndexPath indexPath: NSIndexPath) {
+        print("Tapped \(indexPath.section), \(indexPath.row)")
+    }
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print(indexPath.row)
+        let selectedCell: UITableViewCell = tableView.cellForRowAtIndexPath(indexPath)!
+        if (indexPath.section == 0 && indexPath.row == 2) || (indexPath.section == 1 && indexPath.row != 2) || (indexPath.section == 2) {
+            selectedCell.contentView.backgroundColor = UIColor(red: 240/255, green: 151/255, blue: 103/255, alpha: 1)
+        }
+        
+    }
+    
 }
