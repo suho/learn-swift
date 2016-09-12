5f7b3fb4acb9ac05c4884b4e773d806e889683a6
hovansu <hovansu.it@gmail.com>
2016-09-12 14:29:19 +0700

    - Add Log

--- Project/DemoAlamofireProject/DemoAlamofireProject/Controllers/Home/HomeViewController.swift
+++ Project/DemoAlamofireProject/DemoAlamofireProject/Controllers/Home/HomeViewController.swift
@@ -13,23 +13,22 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        print("Home View Controller")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
 
 }
